6f04ec7ea3fca61a0e09d13507f82e3d0fb8a363
JosphatKimani1 <githuthajosphat@gmail.com>
2026-07-24 16:56:15 +0300

    Removed Hero seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,17 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(AdminDataSeeder::class);
+        $this->call(AdminDataSeeder::class);
         // User::factory(10)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
-        
-    $this->call([
-        HeroSeeder::class,
-    ]);
     }
 }
