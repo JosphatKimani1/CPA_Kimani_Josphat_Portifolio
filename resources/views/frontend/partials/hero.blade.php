8abdd9b703ebc5fa55d38a80b2b0efc9391f559b
JosphatKimani1 <githuthajosphat@gmail.com>
2026-07-24 16:05:49 +0300

    Reverted hero blade to its initial state

--- resources/views/frontend/partials/hero.blade.php
+++ resources/views/frontend/partials/hero.blade.php
@@ -13,32 +13,30 @@
             <div class="row align-items-center">
                <div class="col-md-6">
                   <div class="hero-content-box">
-                     <span class="hero-sub-title">{{ $hero->name ?? '' }}</span>
-                     <h1 class="hero-title">{{ $hero->profession ?? '' }}</h1>
+                     <span class="hero-sub-title">{{ $hero->name }}</span>
+                     <h1 class="hero-title">{{ $hero->profession }}</h1>
 
                      <div class="hero-image-box d-md-none text-center">
-                        <img src="{{ !empty($hero?->photo) 
-                                    ? asset($hero->photo) 
-                                    : asset('uploads/no-img-avatar.png') }}" alt="" />
+                        <img src="{{ $hero->photo ? asset($hero->photo) : asset('uploads/no-img-avatar.png')}}" alt="" />
                      </div>
 
                      <p class="lead">
-                        {{ $hero->short_description ?? '' }}
+                        {{ $hero->short_description }}
                      </p>
                      <div class="button-box d-flex flex-wrap align-items-center">
-                        <a href="{{ $hero->resume ?? '' }}" class="btn tj-btn-secondary">Download CV <i class="flaticon-download"></i></a>
+                        <a href="{{ $hero->resume }}" class="btn tj-btn-secondary">Download CV <i class="flaticon-download"></i></a>
                         <ul class="ul-reset social-icons">
                            <li>
-                              <a href="{{ $hero->twitter_url ?? '' }}"><i class="fa-brands fa-twitter"></i></a>
+                              <a href="{{ $hero->twitter_url }}"><i class="fa-brands fa-twitter"></i></a>
                            </li>
                            <li>
-                              <a href="{{ $hero->youtube_url ?? '' }}"><i class="fa-light fa-basketball"></i></a>
+                              <a href="{{ $hero->youtube_url }}"><i class="fa-light fa-basketball"></i></a>
                            </li>
                            <li>
-                              <a href="{{ $hero->linkedin_url ?? ''}}"><i class="fa-brands fa-linkedin-in"></i></a>
+                              <a href="{{ $hero->linkedin_url }}"><i class="fa-brands fa-linkedin-in"></i></a>
                            </li>
                            <li>
-                              <a href="{{ $hero->github_url ?? ''}}"><i class="fa-brands fa-github"></i></a>
+                              <a href="{{ $hero->github_url }}"><i class="fa-brands fa-github"></i></a>
                            </li>
                         </ul>
                      </div>
@@ -46,9 +44,7 @@
                </div>
                <div class="col-md-6 d-none d-md-block">
                   <div class="hero-image-box text-center">
-                     <img src="{{ !empty($hero?->photo) 
-                                 ? asset($hero->photo) 
-                                 : asset('uploads/no-img-avatar.png') }}" alt="" />
+                     <img src="{{ $hero->photo ? asset($hero->photo) : asset('uploads/no-img-avatar.png')}}" alt="" />
                   </div>
                </div>
             </div>
@@ -56,19 +52,19 @@
                <div class="row">
                   <div class="col-6 col-lg-3">
                      <div class="funfact-item d-flex flex-column flex-sm-row flex-wrap align-items-center">
-                        <div class="number"><span class="odometer" data-count="{{ $hero->YOE ?? '' }}">0</span></div>
+                        <div class="number"><span class="odometer" data-count="{{ $hero->YOE }}">0</span></div>
                         <div class="text">Years of <br />Experience</div>
                      </div>
                   </div>
                   <div class="col-6 col-lg-3">
                      <div class="funfact-item d-flex flex-column flex-sm-row flex-wrap align-items-center">
-                        <div class="number"><span class="odometer" data-count="{{ $hero->PC ?? '' }}">0</span>+</div>
+                        <div class="number"><span class="odometer" data-count="{{ $hero->PC }}">0</span>+</div>
                         <div class="text">Project <br />Completed</div>
                      </div>
                   </div>
                   <div class="col-6 col-lg-3">
                      <div class="funfact-item d-flex flex-column flex-sm-row flex-wrap align-items-center">
-                        <div class="number"><span class="odometer" data-count="{{ $hero->HC ?? '' }}">0</span>K</div>
+                        <div class="number"><span class="odometer" data-count="{{ $hero->HC }}">0</span>K</div>
                         <div class="text">Happy <br />Clients</div>
                      </div>
                   </div>
