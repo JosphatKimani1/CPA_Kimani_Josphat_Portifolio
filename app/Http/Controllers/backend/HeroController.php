35a1fea9723e462700c8ece6730d5aacfb89210d
JosphatKimani1 <githuthajosphat@gmail.com>
2026-07-28 21:36:36 +0300

    added a test code for hero controller

--- app/Http/Controllers/backend/HeroController.php
+++ app/Http/Controllers/backend/HeroController.php
@@ -115,4 +115,11 @@ class HeroController extends Controller
             return redirect()->back()->with($notification);
 
      }//End Method
+
+     public function homepage()
+{
+    $hero = Hero::find(1);
+
+    dd($hero);
+}
 }
